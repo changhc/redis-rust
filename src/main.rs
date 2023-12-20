61c334f53aaaf22fd4cee315ce969ab68a610bdd
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-20 16:51:53 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn parse_request(request: Vec<String>) -> (String, String) {
+    todo!()
+}
+
+fn handle_request(cmd: String, body: String) {
+    todo!()
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let tcp_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let (cmd, body) = parse_request(tcp_request);
+    let result = handle_request(cmd, body);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
