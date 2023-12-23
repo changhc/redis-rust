54552415c41fe809515e0c9e120c3747ca3501c8
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-23 23:22:02 +0100

    update

--- src/command/set.rs
+++ src/command/set.rs
@@ -20,7 +20,7 @@ impl SetCommand {
         }
         Ok(SetCommand {
             key: tokens[0].clone(),
-            value: tokens[0].clone(),
+            value: tokens[1].clone(),
         })
     }
 }
@@ -29,5 +29,58 @@ impl Command for SetCommand {
     fn execute(&self, data_store: &mut HashMap<String, String>) -> Box<dyn ExecutionResult> {
         data_store.insert(self.key.clone(), self.value.clone());
         Box::new(SetResult {})
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use crate::command::Command;
+
+    use super::SetCommand;
+
+    #[test]
+    fn should_accept_exactly_two_tokens() {
+        match SetCommand::new(vec!["foo".to_string()]) {
+            Ok(_) => panic!("should not be ok"),
+            Err(e) => {
+                assert_eq!(
+                    e.to_string(),
+                    "invalid command body. Details: Expected number of tokens: 2, received: 1"
+                        .to_string()
+                );
+            }
+        }
+        match SetCommand::new(vec![
+            "foo".to_string(),
+            "bar".to_string(),
+            "baz".to_string(),
+        ]) {
+            Ok(_) => panic!("should not be ok"),
+            Err(e) => {
+                assert_eq!(
+                    e.to_string(),
+                    "invalid command body. Details: Expected number of tokens: 2, received: 3"
+                        .to_string()
+                );
+            }
+        }
+        match SetCommand::new(vec!["foo".to_string(), "bar".to_string()]) {
+            Ok(v) => {
+                assert_eq!(v.key, "foo".to_string());
+                assert_eq!(v.value, "bar".to_string());
+            }
+            Err(_) => panic!("should not be ok"),
+        }
+    }
+
+    #[test]
+    fn should_insert_value() {
+        let cmd = SetCommand::new(vec!["foo".to_string(), "bar".to_string()]).unwrap();
+        let mut ds = HashMap::<String, String>::new();
+        assert!(ds.get(&"foo".to_string()).is_none());
+        cmd.execute(&mut ds);
+        assert_eq!(ds.get(&"foo".to_string()).unwrap(), &"bar".to_string());
     }
 }
