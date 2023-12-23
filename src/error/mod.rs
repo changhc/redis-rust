8de7d2663677aaafa3d322d7eef1450ea950b3c8
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-23 14:01:55 +0100

    update

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum RequestError {
+    #[error("failed to parse request at `{0}`. Details: {1}")]
+    ParseRequestFailed(String, String),
     #[error("unsupported command `{0}`")]
     UnsupportedCommand(String),
     #[error("unknown request error")]
