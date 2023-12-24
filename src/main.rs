e0a991e5351e26aa3d7809c27a26c655c3275f06
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-24 16:04:47 +0100

    Implement network layer (#7)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn parse_request(request: String) -> (String, String) {
+    todo!()
+}
+
+fn handle_request(cmd: String, body: String) {
+    todo!()
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf_reader = BufReader::new(&stream);
+    let mut request = String::new();
+    if buf_reader.read_line(&mut request).is_err() {
+        return;
+    }
+
+    let (cmd, body) = parse_request(request);
+    let result = handle_request(cmd, body);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
