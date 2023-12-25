609bff76ed37f1f6d91c1f175ac6679e7e0d758d
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-25 21:15:21 +0100

    update

--- src/execution_result/get.rs
+++ src/execution_result/get.rs
@@ -1,14 +1,20 @@
 use super::{ExecutionResult, ResultType};
 
 pub struct GetResult {
-    pub value: String,
+    pub value: Option<String>,
 }
 
 impl ExecutionResult for GetResult {
     fn get_result_type(&self) -> super::ResultType {
-        ResultType::SimpleString
+        match self.value {
+            Some(_) => ResultType::SimpleString,
+            None => ResultType::Null,
+        }
     }
     fn to_string(&self) -> String {
-        self.value.clone()
+        match &self.value {
+            Some(v) => v.clone(),
+            None => "".to_string(),
+        }
     }
 }
