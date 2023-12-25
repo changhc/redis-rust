76a293d8bc967e03288d828c0ff86d624aded371
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-25 10:34:46 +0100

    update

--- src/execution_result/mod.rs
+++ src/execution_result/mod.rs
@@ -2,10 +2,7 @@ mod base;
 pub use base::ExecutionResult;
 mod ping;
 pub use ping::PingResult;
-<<<<<<< HEAD
 mod set;
 pub use set::SetResult;
-=======
->>>>>>> master
 mod types;
 pub use types::ResultType;
