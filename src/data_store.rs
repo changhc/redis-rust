99536872b53a0fd2add4ce5824514d72eaf3e086
Huan-Cheng Chang <changhc84@gmail.com>
2023-12-26 20:58:46 +0100

    update

--- src/data_store.rs
+++ src/data_store.rs
@@ -37,17 +37,12 @@ pub fn get_data_store() -> DataStore {
 
 #[cfg(test)]
 mod test {
-    use super::{get_data_store, RedisEntry, RedisEntryType};
-    use std::collections::LinkedList;
+    use super::{get_data_store, RedisEntry};
 
     #[test]
     fn test_list_store() {
         let mut ds = get_data_store();
-        let s = RedisEntry {
-            type_: RedisEntryType::List,
-            list: Some(LinkedList::<String>::new()),
-            string: None,
-        };
+        let s = RedisEntry::init_list();
         ds.insert("foo".to_string(), s);
 
         let v = ds.get_mut(&"foo".to_string()).unwrap();
