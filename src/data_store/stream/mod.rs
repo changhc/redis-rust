fb9898097f5a8aa5f698be6fcef1c0bcbd411ddb
Huan-Cheng Chang <changhc84@gmail.com>
2024-04-20 13:02:58 +0200

    update

--- src/data_store/stream/mod.rs
+++ src/data_store/stream/mod.rs
@@ -1,3 +1,32 @@
 mod radix_tree;
-use radix_tree::RadixTree;
 mod tree_node;
+
+use crate::error::StreamError;
+use radix_tree::RadixTree;
+use tree_node::TreeNodeId;
+
+pub struct Stream {
+    tree: RadixTree,
+}
+
+impl Stream {
+    pub fn insert(
+        &mut self,
+        id: Option<[u64; 2]>,
+        values: Vec<[String; 2]>,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let new_id = match id {
+            Some(v) => TreeNodeId(v),
+            None => match self.tree.next_available_id(None) {
+                Ok(v) => v,
+                Err(_) => return Err(Box::new(StreamError::IdExhausted)),
+            },
+        };
+
+        if new_id <= *self.tree.top_id() {
+            return Err(Box::new(StreamError::IdNotGreaterThanStreamTop));
+        }
+
+        self.tree.insert(new_id, values)
+    }
+}
